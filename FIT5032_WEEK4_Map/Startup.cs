5411b8bd9453865b0c30db8896b19ff54838f800
JianboYang <jyan0173@student.monash.edu>
2022-09-01 16:32:49 +0800

    Add project files.

---
+++ FIT5032_WEEK4_Map/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_WEEK4_Map.Startup))]
+namespace FIT5032_WEEK4_Map
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
